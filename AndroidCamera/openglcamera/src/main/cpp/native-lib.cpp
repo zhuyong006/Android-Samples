4679a3b3ec6d73ac67321de41ad06566b6d3ac49
zhuyong006 <zhuyong_006@163.com>
2019-04-19 19:19:13 +0800

    up camera fps using opencv

--- AndroidCamera/openglcamera/src/main/cpp/native-lib.cpp
+++ AndroidCamera/openglcamera/src/main/cpp/native-lib.cpp
@@ -1,10 +1,64 @@
 #include <jni.h>
 #include <android/log.h>
+#include <jni.h>
+#include<opencv2/opencv.hpp>
+#include <android/bitmap.h>
+#include<iostream>
+#include <vector>
+using namespace cv;
+using namespace std;
+
+#include <android/log.h>
 #define ALOGE(FORMAT,...) __android_log_print(ANDROID_LOG_ERROR,"Jon",FORMAT,##__VA_ARGS__);
 
+CascadeClassifier *cascade = nullptr;
+
 extern "C"
-JNIEXPORT jstring JNICALL
-Java_com_sunmi_openglcamera_MyGLSurfaceView_stringFromC(JNIEnv *env, jobject instance){
+JNIEXPORT jboolean JNICALL
+Java_com_sunmi_openglcamera_MyGLSurfaceView_initCascade(JNIEnv *env, jobject instance,
+                                                        jstring path_) {
     ALOGE("Java_sunmi_opencv_camera_MainActivity_stringFromC");
-    return env->NewStringUTF("hello NDK From C");
+    const char *cascadePath = env->GetStringUTFChars(path_,0);
+
+    cascade = new CascadeClassifier(cascadePath);
+    if(!cascade)
+    {
+        ALOGE("CascadeClassifier Creat Failed\n");
+        env->ReleaseStringUTFChars(path_, cascadePath);
+        return false;
+    }
+    env->ReleaseStringUTFChars(path_, cascadePath);
+    return true;
+}
+
+extern "C"
+JNIEXPORT void JNICALL
+Java_com_sunmi_openglcamera_MyGLSurfaceView_faceDetect(JNIEnv *env, jobject instance,
+                                                       jbyteArray data_, jint srcFrameWidth,
+                                                       jint srcFrameHeight,jlong addr){
+
+    // TODO
+    Mat gray;
+    jbyte *data = env->GetByteArrayElements(data_, NULL);
+    Mat image = *(Mat *)addr;
+
+    ALOGE("face detect enter E\n");
+
+    cvtColor(image,gray,COLOR_YUV420p2GRAY);
+    std::vector<Rect> rects;
+    cascade->detectMultiScale(gray,rects,1.1,5,0,Size(10,10),Size(300,300));
+    if(rects.empty()) {
+        env->ReleaseByteArrayElements(data_, data, 0);
+        return ;
+    }
+
+    for(int i=0;i<rects.size();i++)
+    {
+        ALOGE("found face\n");
+       // rectangle(image,rects[i],Scalar(255,0,0),2,8,0);
+    }
+    env->ReleaseByteArrayElements(data_, data, 0);
+
+    return ;
+
 }
--- AndroidCamera/openglcamera/src/main/java/com/sunmi/openglcamera/MyGLSurfaceView.java
+++ AndroidCamera/openglcamera/src/main/java/com/sunmi/openglcamera/MyGLSurfaceView.java
@@ -3,21 +3,25 @@ package com.sunmi.openglcamera;
 import android.app.Activity;
 import android.content.Context;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.graphics.ImageFormat;
-import android.graphics.Rect;
 import android.graphics.SurfaceTexture;
-import android.graphics.YuvImage;
 import android.hardware.Camera;
 import android.hardware.Camera.PreviewCallback;
 import android.opengl.GLSurfaceView;
 import android.opengl.GLUtils;
 import android.os.Message;
-import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.SurfaceHolder;
 
-import java.io.ByteArrayOutputStream;
+import org.opencv.android.Utils;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.imgproc.Imgproc;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 import java.nio.FloatBuffer;
@@ -31,7 +35,8 @@ public class MyGLSurfaceView extends GLSurfaceView implements SurfaceHolder.Call
         System.loadLibrary("native-lib");
     }
 
-    public native String stringFromC();
+    public native boolean initCascade(String path);
+    public native void faceDetect(byte[] data, int srcFrameWidth, int srcFrameHeight,long addr);
     //OpenGLES相关
     private int srcFrameWidth  = 640;// 源视频帧宽/高
     private int srcFrameHeight = 480;
@@ -65,15 +70,22 @@ public class MyGLSurfaceView extends GLSurfaceView implements SurfaceHolder.Call
     private SurfaceHolder surfaceHolder;
     private boolean printCharacters=true;
     private SurfaceTexture surfaceTexture;
-
+    private String dstPath = null;
     private Activity activity;
     public int model=0;
-
+    private Bitmap bitmap = null;
     public MyGLSurfaceView(Context context, GetUiHandlerInterface mainUiHandler)
     {
         super(context);
         getUiHandlerInterface=mainUiHandler;
-        stringFromC();
+
+        try {
+            dstPath = getCascadeDir(context);
+        }catch (IOException ioe){
+            Log.e(TAG,ioe.getMessage());
+        }
+        initCascade(dstPath);
+        dstBitmap = Bitmap.createBitmap(640,480,Bitmap.Config.ARGB_8888);
         //设置Renderer到GLSurfaceView
         setRenderer(new MyGL20Renderer());
         // 只有在绘制数据改变时才绘制view
@@ -88,6 +100,24 @@ public class MyGLSurfaceView extends GLSurfaceView implements SurfaceHolder.Call
         surfaceHolder.addCallback(this);
         surfaceHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
     }
+
+    private String getCascadeDir(Context context)throws IOException {
+        InputStream input = getResources().openRawResource(R.raw.lbpcascade_frontalface);
+        File cascadeDir = context.getDir("cascade", Context.MODE_PRIVATE);
+        File file = new File(cascadeDir.getAbsoluteFile(), "lbpcascade_frontalface.xml");
+        String dst = cascadeDir.getAbsoluteFile()+"lbpcascade_frontalface.xml";
+        FileOutputStream output = new FileOutputStream(file);
+        byte[] buff = new byte[1024];
+        int len = 0;
+        while ((len = input.read(buff)) != -1) {
+            output.write(buff, 0, len);
+        }
+
+        input.close();
+        output.close();
+        return dst;
+    }
+
     // 定义一个工具方法，将float[]数组转换为OpenGL ES所需的FloatBuffer
     private FloatBuffer floatBufferUtil(float[] arr)
     {
@@ -264,9 +294,7 @@ public class MyGLSurfaceView extends GLSurfaceView implements SurfaceHolder.Call
             }
             finally
             {
-                // 生成纹理之后，回收位图
-                if (dstBitmap != null)
-                    dstBitmap.recycle();
+
             }
         }
 
@@ -303,8 +331,7 @@ public class MyGLSurfaceView extends GLSurfaceView implements SurfaceHolder.Call
             gl.glDisableClientState(GL10.GL_VERTEX_ARRAY);
             gl.glDisableClientState(GL10.GL_TEXTURE_COORD_ARRAY);
 
-            if (dstBitmap != null)
-                dstBitmap.recycle();
+
         }
         @Override
         public void onSurfaceChanged(GL10 gl, int width, int height)
@@ -338,19 +365,31 @@ public class MyGLSurfaceView extends GLSurfaceView implements SurfaceHolder.Call
     }
 
     private Bitmap yuv2Bitmap(byte[] data, int srcFrameWidth, int srcFrameHeight, int model) {
-        BitmapFactory.Options newOpts = new BitmapFactory.Options();
-        newOpts.inJustDecodeBounds = true;
-        YuvImage yuvimage = new YuvImage(
-                data,
-                ImageFormat.NV21,srcFrameWidth,srcFrameHeight,null);
-        ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        yuvimage.compressToJpeg(new Rect(0, 0, srcFrameWidth, srcFrameHeight), 100, baos);// 80--JPG图片的质量[0-100],100最高
-        byte[] rawImage = baos.toByteArray();
-        //将rawImage转换成bitmap
-        BitmapFactory.Options options = new BitmapFactory.Options();
-        options.inPreferredConfig = Bitmap.Config.ARGB_8888;
-        Bitmap bitmap = BitmapFactory.decodeByteArray(rawImage, 0, rawImage.length, options);
-        return bitmap;
+//        BitmapFactory.Options newOpts = new BitmapFactory.Options();
+//        newOpts.inJustDecodeBounds = true;
+//        YuvImage yuvimage = new YuvImage(
+//                data,
+//                ImageFormat.NV21,srcFrameWidth,srcFrameHeight,null);
+//        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+//        yuvimage.compressToJpeg(new Rect(0, 0, srcFrameWidth, srcFrameHeight), 100, baos);// 80--JPG图片的质量[0-100],100最高
+//        byte[] rawImage = baos.toByteArray();
+//        //将rawImage转换成bitmap
+//        BitmapFactory.Options options = new BitmapFactory.Options();
+//        options.inPreferredConfig = Bitmap.Config.ARGB_8888;
+//        Bitmap bitmap = BitmapFactory.decodeByteArray(rawImage, 0, rawImage.length, options);
+
+        Mat image = new Mat((int)(srcFrameHeight*1.5),srcFrameWidth, CvType.CV_8UC1);
+        image.put(0,0,data);
+        Mat bitmap = new Mat();
+        long addr = image.getNativeObjAddr();
+        faceDetect(data,srcFrameWidth,srcFrameHeight,addr);
+
+        Imgproc.cvtColor(image , bitmap, Imgproc.COLOR_YUV420sp2RGBA);//转换颜色空间
+
+        Utils.matToBitmap(bitmap,dstBitmap);
+        image.release();
+        bitmap.release();
+        return dstBitmap;
     }
 
 
